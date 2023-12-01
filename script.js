aa256d96723cde3fb19efa817760cc0becf5bbf7
zainkhan80 <zainkhan081105@gmail.com>
2023-12-02 01:36:54 +0500

    Work Complete

---
+++ script.js
@@ -0,0 +1,17 @@
+var a =Number(prompt("Insert your first sub No"))
+
+
+var b =Number(prompt("Insert your second sub No"))
+
+
+var c =Number(prompt("Insert your third sub No"))
+
+
+var d =Number(prompt("Insert your Fourth sub No"))
+
+
+var c =Number(prompt("Insert your fifth sub No"))
+
+
+alert (a+b+c+d)
+
